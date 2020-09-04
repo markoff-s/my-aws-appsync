6554ab2cc37455803ba6121dde86fc76b6e09aa2
Zac H <zac.haluza@gmail.com>
2020-09-04 18:09:08 -0400

    set up app for basic CRUD (w/ outdated schema)

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import Form from './components/Form';
+import Artist from './components/Artist';
+import { ThemeProvider } from 'styled-components';
+import API from '@aws-amplify/api';
+import theme from './styled-components/Theme';
+import './scss/app.scss';
 
-function App() {
-  return <div className="App">Hello app</div>;
+interface Artist {
+  id: string;
+  name: string;
+  description: string;
 }
+
+const App = () => {
+  const [artists, setArtists] = useState([]);
+
+  const fetchArtists = async () => {
+    try {
+      const artistsData: any = await API.graphql({ query: ListArtists });
+      setArtists(artistsData.data.listArtists.items);
+    } catch (err) {
+      console.log('error: ', err);
+    }
+  };
+  useEffect(() => {
+    fetchArtists();
+  }, []);
+
+  useEffect(() => {
+    console.log(artists);
+  }, [artists]);
+
+  return (
+    <ThemeProvider theme={theme}>
+      <div className="app">
+        <h2>Add and update artists</h2>
+        <Form setArtists={setArtists} />
+        {artists && (
+          <div className="artist-grid">
+            {artists.map((artist: Artist) => (
+              <Artist
+                key={artist.id}
+                id={artist.id}
+                artistName={artist.name}
+                artistDescription={artist.description}
+                setArtists={setArtists}
+              />
+            ))}
+          </div>
+        )}
+      </div>
+    </ThemeProvider>
+  );
+};
 
 export default App;
