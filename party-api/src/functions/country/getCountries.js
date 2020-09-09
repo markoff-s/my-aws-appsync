5eb5298e556fc433fefa20d8d2e7a5fb8da53fcd
Slava Markov <xz>
2020-09-09 09:56:52 -0400

    get secrets from SecretsManager for db calls

--- party-api/src/functions/country/getCountries.js
+++ party-api/src/functions/country/getCountries.js
@@ -1,24 +1,65 @@
-'use strict';
+exports.getCountries = async (event, context, callback) => {
 
-exports.getCountries = (event, context, callback) => {
-	console.log('Received event {}', JSON.stringify(event, 3));
+	// Load the AWS SDK
+	var AWS = require('aws-sdk'),
+		region = "us-east-1",
+		secretName = "mydb-postgres";
 
-	console.log('Got an Invoke Request.');
+	// Create a Secrets Manager client
+	var smClient = new AWS.SecretsManager({
+		region: region
+	});
+	const smData = await smClient.getSecretValue({ SecretId: secretName }).promise();
+	const secret = JSON.parse(smData.SecretString);
 
-	const countries = [
-		{
-			id: 1,
-			name: "Canada"
-		},
-		{
-			id: 2,
-			name: "UK"
-		},
-		{
-			id: 3,
-			name: "US"
-		},
-	]
+	const { Client } = require('pg');  //  Needs the nodePostgres Lambda Layer.
+	const client = new Client({
+		user: secret.username,
+		host: secret.host,
+		database: secret.dbname,
+		password: secret.password,
+		port: secret.port,
+	});
+	await client.connect();
 
-	callback(null, countries);
-};
+	const data = await client.query(`
+	  select id, name
+	  from mydb.public.country
+	  order by name asc;
+	`);
+	await client.end();
+
+	callback(null, { rows: data.rows });
+	// const response = {
+	// 	statusCode: 200,
+	// 	body: {
+	// 	  id: res.rows[0].timestamp.valueOf(),
+	// 	  isoccupied: res.rows[0].isoccupied
+	// 	},
+	// };
+	// return response;
+}
+
+
+// exports.getCountries = (event, context, callback) => {
+// 	console.log('Received event {}', JSON.stringify(event, 3));
+
+// 	console.log('Got an Invoke Request.');
+
+// 	const countries = [
+// 		{
+// 			id: 1,
+// 			name: "Canada"
+// 		},
+// 		{
+// 			id: 2,
+// 			name: "UK"
+// 		},
+// 		{
+// 			id: 3,
+// 			name: "US"
+// 		},
+// 	]
+
+// 	callback(null, countries);
+// };
