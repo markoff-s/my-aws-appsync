1cb43dd39d88c1fe537993f11269c1c2036fe10c
Zac H <zac.haluza@gmail.com>
2020-09-10 19:42:59 -0400

    create container to house add group/artist comps

---
+++ client/src/components/grow-sprint-2/AddContainer.tsx
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import Button from '../../styled-components/Button';
+import AddArtist from './AddArtist';
+import AddGroup from './AddGroup';
+import { Group, Person } from '../../types/ArtistTypes';
+
+interface Props {
+  handleDisplayReset: () => void;
+  setGroups: React.Dispatch<React.SetStateAction<Group[]>>;
+  setPersons: React.Dispatch<React.SetStateAction<Person[]>>;
+}
+
+const AddContainer: React.FC<Props> = ({ handleDisplayReset, setGroups, setPersons }) => {
+  const [toggleArtist, setToggleArtist] = useState(true);
+  const [toggleGroup, setToggleGroup] = useState(false);
+
+  const handleToggle = () => {
+    setToggleArtist((prevState) => !prevState);
+    setToggleGroup((prevState) => !prevState);
+  };
+
+  return (
+    <div className="add-container">
+      <Button onClick={handleDisplayReset} color="light">
+        Back to Search
+      </Button>
+      <div className="buttons">
+        <Button onClick={handleToggle}>Add Artist</Button>
+        <Button onClick={handleToggle}>Add Group</Button>
+      </div>
+      {toggleArtist && <AddArtist setPersons={setPersons} />}
+      {toggleGroup && <AddGroup setGroups={setGroups} />}
+    </div>
+  );
+};
+
+export default AddContainer;
