61c3adfba9695ceee1dc7cb507f8859dbff67754
Zac H <zac.haluza@gmail.com>
2020-09-09 20:54:57 -0400

    switch Amplify config to use cognito for auth

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -5,14 +5,26 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import Amplify from 'aws-amplify';
 
-// Temporarily switching API from Cognito auth to API key for simpler testing
+// Cognito access
 Amplify.configure({
   aws_project_region: 'us-east-1',
   aws_appsync_graphqlEndpoint: process.env.REACT_APP_APPSYNC_ENDPOINT,
   aws_appsync_region: 'us-east-1',
-  aws_appsync_authenticationType: 'API_KEY',
-  aws_appsync_apiKey: process.env.REACT_APP_APPSYNC_API_KEY,
+  aws_appsync_authenticationType: 'AMAZON_COGNITO_USER_POOLS',
+  aws_cognito_identity_pool_id: process.env.REACT_APP_COGNITO_IDENTITY_POOL_ID,
+  aws_user_pools_id: process.env.REACT_APP_USER_POOLS_ID,
+  aws_user_pools_web_client_id: process.env.REACT_APP_USER_POOLS_WEB_CLIENT_ID,
+  oauth: {},
 });
+
+// API key access
+// Amplify.configure({
+//   aws_project_region: 'us-east-1',
+//   aws_appsync_graphqlEndpoint: process.env.REACT_APP_APPSYNC_ENDPOINT,
+//   aws_appsync_region: 'us-east-1',
+//   aws_appsync_authenticationType: 'API_KEY',
+//   aws_appsync_apiKey: process.env.REACT_APP_APPSYNC_API_KEY,
+// });
 
 ReactDOM.render(
   <React.StrictMode>
