8d7d72f773e4154dda91e7083fd69f69e41ba8b7
Zac H <zac.haluza@gmail.com>
2020-09-11 08:28:03 -0400

    un-comment out groups data logic, add error state

--- client/src/components/grow-sprint-2/MainContainer.tsx
+++ client/src/components/grow-sprint-2/MainContainer.tsx
@@ -24,19 +24,18 @@ const MainContainer = () => {
   const [displayAddScreen, setDisplayAddScreen] = useState(false);
   const [currentGroupData, setCurrentGroupData] = useState<Group | null>(null);
   const [currentPersonData, setCurrentPersonData] = useState<Person | null>();
+  const [error, setError] = useState(false);
 
   // Grab all group & person data (can later refactor for scalability)
   useEffect(() => {
-    console.log('initializing filtered results');
-    // fetchGroups();
+    fetchGroups();
     fetchPersons();
   }, []);
 
   // update filtered results based on search string
   useEffect(() => {
-    console.log('updating filtered results');
-    if (searchTerm === '') {
-      // fetchGroups();
+    if (searchTerm !== '') {
+      fetchGroups();
       fetchPersons();
     } else updateResults();
   }, [searchTerm]);
@@ -45,7 +44,7 @@ const MainContainer = () => {
   useEffect(() => {
     if (searchTerm.trim() === '') {
       setDisplayResults(false);
-      // setFilteredGroups([]);
+      setFilteredGroups([]);
       setFilteredPersons([]);
     } else setDisplayResults(true);
   }, [searchTerm]);
@@ -53,13 +52,15 @@ const MainContainer = () => {
   // log current persons data
   useEffect(() => {
     console.log({ persons });
-  }, [persons]);
+    console.log({ groups });
+  }, [persons, groups]);
 
   async function fetchGroups() {
     try {
       const groupsData: any = await API.graphql({ query: queries.groups });
       setGroups(groupsData.data.groups);
     } catch (err) {
+      setError(true);
       console.log('error: ', err);
     }
   }
@@ -69,8 +70,8 @@ const MainContainer = () => {
       const personsData: any = await API.graphql({ query: queries.artists });
       console.log(personsData.data.artists);
       setPersons(personsData.data.artists);
-      console.log('got artist data');
     } catch (err) {
+      setError(true);
       console.log('error: ', err);
     }
   }
@@ -79,10 +80,10 @@ const MainContainer = () => {
   // TODO: Refactor so that search only occurs when user explicitly clicks/taps button
   function updateResults() {
     const regex = new RegExp(searchTerm, 'i');
-    // const updatedGroups = groups.filter((group) => group.name.match(regex));
+    const updatedGroups = groups.filter((group) => group.name.match(regex));
     const updatedPersons = persons.filter((person) => person.name.match(regex));
     console.log(updatedPersons);
-    // setFilteredGroups(updatedGroups);
+    setFilteredGroups(updatedGroups);
     setFilteredPersons(updatedPersons);
   }
 
@@ -122,6 +123,7 @@ const MainContainer = () => {
         writers, producers, engineers, musicians, and other contributors.
       </p>
       <Button onClick={() => setDisplayAddScreen(true)}>Add Artist or Group</Button>
+      {error && <h2>Nothing Found - Please Adjust Your Search</h2>}
       {displayAddScreen && !displayGroupScreen && !displayPersonScreen && (
         <AddContainer
           setGroups={setGroups}
