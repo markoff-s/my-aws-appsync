9cab36da5059fda4e79964a352addb241f99b6f2
Zac H <zac.haluza@gmail.com>
2020-09-11 21:47:47 -0400

    add new component to handle mock login flow

---
+++ client/src/containers/AppContainer.tsx
@@ -0,0 +1,23 @@
+import React, { Fragment } from 'react';
+
+import OktaLogin from '../components/OktaLogin';
+import MainContainer from './MainContainer';
+import Header from '../components/Header';
+
+interface Props {
+  isLoggedIn: boolean;
+  setIsLoggedIn: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+const AppContainer: React.FC<Props> = ({ isLoggedIn, setIsLoggedIn }) => {
+  return !isLoggedIn ? (
+    <OktaLogin setIsLoggedIn={setIsLoggedIn} />
+  ) : (
+    <Fragment>
+      <Header isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />
+      <MainContainer />
+    </Fragment>
+  );
+};
+
+export default AppContainer;
