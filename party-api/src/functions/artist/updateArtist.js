cd36239f2f7390368c91213bb5a487f9a0d9af8d
Slava Markov <xz>
2020-09-14 14:41:08 -0400

    updateArtist updates artist's groups

--- party-api/src/functions/artist/updateArtist.js
+++ party-api/src/functions/artist/updateArtist.js
@@ -15,23 +15,36 @@ exports.updateArtist = async (event, context, callback) => {
 	await client.connect();
 
 	try {
-		const input = event.arguments.input;
+		// update artist
+		const artist = event.arguments.input;
 		const data = await client.query(`
 		update person 
 		set name = $1, type = $2, dob = $3, country_id =$4	
 		where id = $5;
-	`, 
-	[
-		input.name, 
-		input.type === 'NATURAL_PERSON' ? 1 : 2, 
-		input.dob, 
-		input.countryId, 
-		input.id
-	]);
+	`,
+			[
+				artist.name,
+				artist.type === 'NATURAL_PERSON' ? 1 : 2,
+				artist.dob,
+				artist.countryId,
+				artist.id
+			]);
 
-	callback(null, input);
+		// update artist's groups
+		if (artist.groups && artist.groups.length > 0) {
+			const valuesString = artist.groups.map(groupId => { return `(${groupId}, ${artist.id})` }).join(", ");
+			// console.log(valuesString);
+
+			await client.query(`
+			delete from group_member where person_id = ${artist.id};
+			insert into group_member
+			values ${valuesString};
+			`);
+		}
+
+		callback(null, artist);
 	}
 	finally {
 		await client.end();
-	}	
+	}
 }
