65e7a71e823410f3753acffc1c67f3aec2060b3f
Zac H <zac.haluza@gmail.com>
2020-09-14 15:52:49 -0400

    add search button

--- client/src/containers/SearchContainer.tsx
+++ client/src/containers/SearchContainer.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Card from '../styled-components/Card';
-
+import Button from '../styled-components/Button';
+import Input from '../styled-components/Input';
 interface Props {
   searchTerm: string;
   setSearchTerm: React.Dispatch<React.SetStateAction<string>>;
@@ -12,12 +13,13 @@ const SearchContainer: React.FC<Props> = ({ searchTerm, setSearchTerm }) => {
       <select name="" id="">
         <option value="">Open Search</option>
       </select>
-      <input
+      <Input
         type="text"
         placeholder="Search Artists, ISNI, UAID"
         value={searchTerm}
         onChange={(e) => setSearchTerm(e.target.value)}
       />
+      <Button>Search</Button>
     </Card>
   );
 };
